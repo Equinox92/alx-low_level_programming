e7aae3497febd40fa8d8a6e5359798e99fed74df
Equinox92 <aklymanuel@outlook.com>
2023-09-08 18:28:58 -0700

    Shell redirections

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - main entry point
+return: always 0 (success)
+ * this is a description of the function
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

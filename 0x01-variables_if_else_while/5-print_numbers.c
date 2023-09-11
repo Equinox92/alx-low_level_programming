ffacb7d8e6d832a27e0a1bed10cb00db9a05102f
Equinox92 <aklymanuel@outlook.com>
2023-09-11 14:46:16 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: 'Print base 10 numbers'
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
+

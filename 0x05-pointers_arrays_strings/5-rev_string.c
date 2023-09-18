19f0e0b3f05f9655004190c8c26c6ece55a89a6d
Equinox92 <aklymanuel@outlook.com>
2023-09-18 15:18:25 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  *rev_string - outputs a reversed string
+  *@s: Input
+  *Return: Reversed string
+  */
+void rev_string(char *s)
+{
+	int a;
+	int c = 0;
+	char r = s[0];
+
+	while (s[c] != '\0')
+		c++;
+	for (a = 0; a < c; a++)
+	{
+		c--;
+		r =s[a];
+		s[a] = s[c];
+		s[c] = r;
+	}
+
+}

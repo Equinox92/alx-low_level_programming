6a9443570e639ddffb046456bdeeffe72ce36448
Equinox92 <aklymanuel@outlook.com>
2023-09-18 15:35:21 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  *puts2 - selects one character to output beginning 
+  *with the first
+  *@str: input
+  *Return: print output
+  */
+void puts(char *str)
+{
+	int a;
+	int ls = 0;
+	int snd = 0;
+	char *b = str;
+
+	while (*b != '\0')
+	{
+		b++;
+		ls++;
+	}
+	snd = ls - 1;
+	for (a = 0 ; a <= snd ; a++)
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a])
+	}
+	}
+	_putchar('\n');
+}
